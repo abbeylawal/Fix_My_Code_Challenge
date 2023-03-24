4d475c05ab45f632b085af068f198c29123bf5e1
abbeylawal <lawalabbeysmith@gmail.com>
2023-03-24 17:01:55 +0100

    added print_square.js

---
+++ 0x00-challenge/1-print_square.js
@@ -0,0 +1,9 @@
+function printSquare(size) {
+    for (let i = 0; i < size; i++) {
+      let row = '';
+      for (let j = 0; j < size; j++) {
+        row += '#';
+      }
+      console.log(row);
+    }
+}
