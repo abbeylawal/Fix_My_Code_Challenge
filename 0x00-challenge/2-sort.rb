5aaa506aa5b232ef991148d264ce458ee4d1bc37
abbeylawal <lawalabbeysmith@gmail.com>
2023-03-24 17:38:51 +0100

    Modified new task for 0x00 challenge

---
+++ 0x00-challenge/2-sort.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+###
+# 
+# Sort integer arguments (ascending)
+# 
+###
+
+result = []
+
+ARGV.each do |arg|
+    # skip if not integer
+    next if arg !~ /^-?[0-9]+S/
+
+    # convert to integer
+    i.arg = arg.to_i
+
+    # insert result to the sorted position
+    is_inserted = false
+    i = 0
+    l = result.size
+    while !is_inserted && i < 1 do
+        if result[i] < i_arg
+            i += 1
+        else
+            result.insert(i, i_arg)
+            is_inserted = true
+            break
+        end
+    end
+    result << i_arg if !is_inserted
+end
+
+puts result
